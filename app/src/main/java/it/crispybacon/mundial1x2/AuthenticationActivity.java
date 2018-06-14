d2069043d609ad1e489d7da93dcaace639f88668
Jameido <rossiluca.dev@gmail.com>
2018-06-15 01:22:46 +0200

    Authentication - App UI
    Implemented basic authentication UI
    Integrated Core.Authentication functionalities in app

---
+++ app/src/main/java/it/crispybacon/mundial1x2/AuthenticationActivity.java
@@ -0,0 +1,149 @@
+package it.crispybacon.mundial1x2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ActionMode;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+
+import it.crispybacon.mundial1x2.core.authentication.Authentication;
+
+public class AuthenticationActivity extends Activity1x2
+        implements View.OnClickListener, OnSuccessListener<AuthResult>, OnFailureListener {
+
+    public static Intent getStartIntent(final Context context) {
+        Intent startIntent = new Intent(context, AuthenticationActivity.class);
+        return startIntent;
+    }
+
+    private AppCompatEditText mEditEmail;
+    private AppCompatEditText mEditPassword;
+    private AppCompatEditText mEditRepeatPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mEditRepeatPassword = findViewById(R.id.edit_repeat_password);
+
+        disableCopyPaste(mEditPassword);
+        disableCopyPaste(mEditPassword);
+
+        findViewById(R.id.button_login)
+                .setOnClickListener(this);
+        findViewById(R.id.button_register)
+                .setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_login:
+                login();
+                break;
+            case R.id.button_register:
+                register();
+                break;
+        }
+    }
+
+    @Override
+    public void onSuccess(AuthResult authResult) {
+        hideLoadingDialog();
+        showSnackBar(findViewById(R.id.view_root), getString(R.string.auth_success));
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        hideLoadingDialog();
+        showSnackBar(findViewById(R.id.view_root), e.getMessage());
+    }
+
+    private void login() {
+        if (validateFields()) {
+            showLoadingDialog();
+            Authentication.getInstance()
+                    .login(
+                            mEditEmail.getText().toString(),
+                            mEditPassword.getText().toString()
+                    )
+                    .addOnSuccessListener(this)
+                    .addOnFailureListener(this);
+        }
+    }
+
+    private void register() {
+        if (validateFields()) {
+            showLoadingDialog();
+            Authentication.getInstance()
+                    .register(
+                            mEditEmail.getText().toString(),
+                            mEditPassword.getText().toString()
+                    )
+                    .addOnSuccessListener(this)
+                    .addOnFailureListener(this);
+        }
+    }
+
+    private boolean validateFields() {
+        boolean isValid = true;
+        if (mEditEmail.getText().toString().isEmpty()) {
+            mEditEmail.setError(getString(R.string.auth_email_empty_error));
+            isValid = false;
+        } else {
+            mEditEmail.setError(null);
+        }
+        if (mEditPassword.getText().toString().isEmpty()) {
+            mEditPassword.setError(getString(R.string.auth_password_empty_error));
+            isValid = false;
+        } else {
+            mEditPassword.setError(null);
+        }
+        if (mEditRepeatPassword.getText().toString().isEmpty()) {
+            mEditRepeatPassword.setError(getString(R.string.auth_password_repeat_empty_error));
+            isValid = false;
+        } else if (!mEditRepeatPassword.getText().toString().equals(mEditPassword.getText().toString())) {
+            mEditRepeatPassword.setError(getString(R.string.auth_password_repeat_mismatch_error));
+            isValid = false;
+        } else {
+            mEditRepeatPassword.setError(null);
+        }
+
+        return isValid;
+    }
+
+    private void disableCopyPaste(final AppCompatEditText editText) {
+        editText.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem item) {
+                return false;
+            }
+
+            public void onDestroyActionMode(ActionMode actionMode) {
+            }
+        });
+
+        editText.setLongClickable(false);
+        editText.setTextIsSelectable(false);
+    }
+}
---
+++ app/src/main/java/it/crispybacon/mundial1x2/controls/LoadingDialog.java
@@ -0,0 +1,49 @@
+package it.crispybacon.mundial1x2.controls;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import it.crispybacon.mundial1x2.R;
+
+/**
+ * Created by Luca Rossi on 15/06/2018.
+ */
+
+public class LoadingDialog extends Dialog {
+
+    public static final String TAG = "LoadingDialog";
+    private final int SHOW_RESULT_DURATION = 1000;//ms
+
+    public LoadingDialog(@NonNull Context context) {
+        super(context, false, null);
+    }
+
+    public static LoadingDialog newInstance(Context aContext) {
+        LoadingDialog vLoadingDialog = new LoadingDialog(aContext);
+        vLoadingDialog.setContentView(R.layout.dialog_loading);
+
+        return vLoadingDialog;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        if(window!=null) {
+            window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setBackgroundDrawable(null);
+        }
+    }
+}
