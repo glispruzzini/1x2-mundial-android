5ca85269eee0dd6cfd3195965728140fe2bff75c
itscap <enrico.cappozzo@gmail.com>
2018-06-15 17:08:08 +0200

    BentBackgroundLayout fixed

--- app/src/main/java/it/crispybacon/mundial1x2/ui/section/BentBackgroundLayout.java
+++ app/src/main/java/it/crispybacon/mundial1x2/ui/section/BentBackgroundLayout.java
@@ -21,15 +21,9 @@ public class BentBackgroundLayout extends RelativeLayout {
     private static final String TAG = "BentBackgroundLayout";
 
     private Paint mFillerPaint;
-    private Path mFillPath;
-    private Point mTopLeft;
-    private Point mTopRight;
-    private Point mBottomRight;
-    private Point mBottomLeft;
     private int mWidth;
     private int mHeight;
     private int mArcHeight;
-    private int translationY;
 
     public BentBackgroundLayout(Context context) {
         super(context);
@@ -63,53 +57,17 @@ public class BentBackgroundLayout extends RelativeLayout {
                 mFillerPaint
         );
         canvas.drawRect(0,mArcHeight/2, mWidth,mHeight,mFillerPaint);
-        /*
-        initPoints(canvas);
-        mFillPath.reset();
-        mFillPath.moveTo(mTopLeft.x, mTopLeft.y);
-        mFillPath.lineTo(mTopRight.x, mTopRight.y);
-        mFillPath.lineTo(mBottomRight.x, mBottomRight.y);
-        mFillPath.lineTo(mBottomLeft.x, mBottomLeft.y);
-        mFillPath.close();
-
-        canvas.drawPath(mFillPath, mFillerPaint);
-        */
     }
 
     private void init(AttributeSet attrs) {
 
         setWillNotDraw(false);
         mArcHeight = MeasureHelper.getPointsValue(getContext(), 120);
-        translationY = mArcHeight/4;
 
         mFillerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mFillerPaint.setColor(getResources().getColor(R.color.white));
 
-        setTranslationY(-translationY);
-        mFillPath = new Path();
-        mTopLeft = new Point();
-        mTopRight = new Point();
-        mBottomRight = new Point();
-        mBottomLeft = new Point();
-
     }
 
-    private void initPoints(Canvas canvas) {
 
-        mTopLeft.set(0,mArcHeight/2);
-        mTopRight.set(canvas.getWidth(), mArcHeight/2);
-        mBottomRight.set(canvas.getWidth(), canvas.getHeight()+mArcHeight/2);
-        mBottomLeft.set(0, canvas.getHeight()+mArcHeight/2);
-    }
-
-    public void setupPosition(){
-
-        setTranslationY(-translationY);
-
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) getLayoutParams();
-        params.height = mHeight+translationY;
-        setLayoutParams(params);
-
-        invalidate();
-    }
 }
