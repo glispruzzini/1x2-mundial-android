1d14d7290e939a99225dbd73ef71ee8ff6742dff
itscap <enrico.cappozzo@gmail.com>
2018-06-16 20:09:11 +0200

    ScoreViewHolder linked to layout

--- app/src/main/java/it/crispybacon/mundial1x2/sections/score/ScoreAdapter.java
+++ app/src/main/java/it/crispybacon/mundial1x2/sections/score/ScoreAdapter.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import it.crispybacon.mundial1x2.R;
 import it.crispybacon.mundial1x2.ui.imageview.FlagImageView;
+import it.crispybacon.mundial1x2.ui.imageview.RoundedImageView;
 import it.crispybacon.mundial1x2.ui.text.DateTextView;
 
 /**
@@ -36,24 +37,24 @@ public class ScoreAdapter extends RecyclerView.Adapter<ScoreAdapter.ScoreViewHol
 
     class ScoreViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private FlagImageView mFlagImageLeft;
-        private FlagImageView mFlagImageRight;
-        private DateTextView mDateTextView;
-        private AppCompatTextView mHourTextView;
+        private AppCompatTextView mTextPos;
+        private AppCompatTextView mTextName;
+        private AppCompatTextView mTextScore;
+        private RoundedImageView mProfilePic;
 
         public ScoreViewHolder(View itemView) {
             super(itemView);
 
-            mFlagImageLeft = itemView.findViewById(R.id.img_left_team);
-            mFlagImageRight = itemView.findViewById(R.id.img_right_team);
-            mDateTextView = itemView.findViewById(R.id.text_date);
-            mHourTextView = itemView.findViewById(R.id.text_hour);
+            mTextPos = itemView.findViewById(R.id.text_pos);
+            mProfilePic = itemView.findViewById(R.id.img_profile);
+            mTextName = itemView.findViewById(R.id.text_name);
+            mTextScore = itemView.findViewById(R.id.text_score);
 
         }
 
         public void bind(Score aScore) {
 
-
+        //TODO
 
         }
 
